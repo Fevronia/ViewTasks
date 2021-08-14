55c055943051c9ffcfd49008663fddefd377ddab
Anastasia Lutchenko <nasta.lutchenko@gmail.com>
2021-08-14 18:12:44 +0300

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewTasks.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SprintTasks()
+        {
+            return View();
+        }
+        public IActionResult Greetings()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Supermarkets()
+        {
+            return View();
+        }
+        public IActionResult ShoppingList()
+        {
+           
+            return View();
+        }
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+        public IActionResult TimeToBuy()
+        {
+            return PartialView("_TimeToBuy");
+        }
+
+    }
+}
